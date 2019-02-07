32b046532ef6b8450468779efeab20992eaacf8f
Florian <florian-1992@hotmail.fr>
2019-02-07 14:38:58 +0100

    Starting project tracking

---
+++ Content/Collection/New Zealand Places.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="New Zealand Places" FOLDED="false" ID="ID_299154900" CREATED="1549276366098" MODIFIED="1549280565723" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.5" layout="OUTLINE">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Location" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_1074518566" CREATED="1549277805405" MODIFIED="1549280560206" HGAP_QUANTITY="242.2499927803876 pt" VSHIFT_QUANTITY="-75.74999774247415 pt" STYLE="fork">
+<hook NAME="FreeNode"/>
+<edge COLOR="#00007c"/>
+<node TEXT="Maps" ID="ID_65297510" CREATED="1549278704587" MODIFIED="1549280560207" HGAP_QUANTITY="49.249998949468164 pt" VSHIFT_QUANTITY="-5.249999843537811 pt" STYLE="fork"/>
+<node TEXT="Region" ID="ID_1937545095" CREATED="1549278716713" MODIFIED="1549280560208" HGAP_QUANTITY="47.749998994171655 pt" VSHIFT_QUANTITY="-1.4999999552965164 pt" STYLE="fork"/>
+<node TEXT="Name" ID="ID_1960961653" CREATED="1549278730480" MODIFIED="1549280560208" HGAP_QUANTITY="47.74999899417165 pt" VSHIFT_QUANTITY="-17.249999485909953 pt" STYLE="fork"/>
+</node>
+<node TEXT="Nature" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_426352377" CREATED="1549277702294" MODIFIED="1549280560204" HGAP_QUANTITY="-30.749999083578643 pt" VSHIFT_QUANTITY="-72.7499978318811 pt" STYLE="fork">
+<hook NAME="FreeNode"/>
+<edge COLOR="#7c0000"/>
+<node TEXT="Weather" ID="ID_938997637" CREATED="1549278570176" MODIFIED="1549280560208" HGAP_QUANTITY="-102.9999965131284 pt" VSHIFT_QUANTITY="0.0 pt" STYLE="fork"/>
+<node TEXT="Geology" ID="ID_1071223559" CREATED="1549278590697" MODIFIED="1549280560208" HGAP_QUANTITY="-102.99999651312838 pt" VSHIFT_QUANTITY="-4.499999865889553 pt" STYLE="fork"/>
+<node TEXT="Wildlife" ID="ID_434008763" CREATED="1549278608541" MODIFIED="1549280560208" HGAP_QUANTITY="-102.99999651312838 pt" VSHIFT_QUANTITY="-0.7499999776482591 pt" STYLE="fork"/>
+<node TEXT="Pictures" ID="ID_199682794" CREATED="1549278637974" MODIFIED="1549280560209" HGAP_QUANTITY="-102.9999965131284 pt" VSHIFT_QUANTITY="-5.249999843537812 pt" STYLE="fork"/>
+</node>
+<node TEXT="Economy" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_1969045307" CREATED="1549278104892" MODIFIED="1549281068589" HGAP_QUANTITY="-90.7499972954393 pt" VSHIFT_QUANTITY="65.24999805539854 pt" STYLE="fork">
+<hook NAME="FreeNode"/>
+<edge COLOR="#007c00"/>
+<node TEXT="Function of the place" ID="ID_1296121336" CREATED="1549278408259" MODIFIED="1549281097281" HGAP_QUANTITY="-107.49999637901794 pt" VSHIFT_QUANTITY="16.499999508261695 pt" STYLE="fork"/>
+<node TEXT="Job opportunities" ID="ID_137029514" CREATED="1549281098633" MODIFIED="1549281151433"/>
+<node TEXT="Population" ID="ID_1594761362" CREATED="1549281152663" MODIFIED="1549281167125"/>
+<node TEXT="Services" ID="ID_347108072" CREATED="1549281157707" MODIFIED="1549281171823"/>
+<node TEXT="Housing" ID="ID_794723922" CREATED="1549281158877" MODIFIED="1549281175575"/>
+</node>
+<node TEXT="Social" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_797129273" CREATED="1549277888719" MODIFIED="1549281074803" HGAP_QUANTITY="254.99999240040802 pt" VSHIFT_QUANTITY="81.74999756366022 pt" STYLE="fork">
+<hook NAME="FreeNode"/>
+<node TEXT="Description" ID="ID_570760910" CREATED="1549278792571" MODIFIED="1549281111315" HGAP_QUANTITY="53.74999881535772 pt" VSHIFT_QUANTITY="2.9999999105930373 pt" STYLE="fork">
+<node TEXT="History" ID="ID_1246435897" CREATED="1549281112903" MODIFIED="1549281119376"/>
+<node TEXT="Politics" ID="ID_646457176" CREATED="1549281124174" MODIFIED="1549281131601"/>
+<node TEXT="Atmosphere" ID="ID_1812340197" CREATED="1549281125185" MODIFIED="1549281136884"/>
+</node>
+</node>
+</node>
+</map>
